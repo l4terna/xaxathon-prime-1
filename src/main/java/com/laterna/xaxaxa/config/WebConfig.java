af5ebb457072f625dfda3223e80eff0e141b5531
saintgottenn <satem3663@gmail.com>
2024-11-23 01:49:30 +0300

    edit cors

--- src/main/java/com/laterna/xaxaxa/config/WebConfig.java
+++ src/main/java/com/laterna/xaxaxa/config/WebConfig.java
@@ -6,11 +6,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-   @Override
-   public void addCorsMappings(CorsRegistry registry) {
-       registry.addMapping("/**")
-           .allowedOrigins("*")
-           .allowedMethods("*")
-           .allowedHeaders("*");
-   }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "Content-Type", "X-Total-Count")
+                .allowCredentials(false)  // должно быть false при allowedOrigins("*")
+                .maxAge(3600L); // время кэширования CORS конфигурации в секундах
+    }
 }
